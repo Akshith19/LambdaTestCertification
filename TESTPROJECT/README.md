2dc3d741fa58b68b1d499544b73e2b4b749018ae
Akshith <akshithrao19@gmail.com>
2023-06-26 23:04:00 +0530

    First commit

---
+++ TESTPROJECT/README.md
@@ -0,0 +1,11 @@
+WEB AUTOMATION
+
+Steps to run the code
+1. Through testng.xml file
+2. Through maven commands ( mvn test)
+
+Classes:
+1. TestBase class:
+	Which has the capability setup method where it requires browser name as a arguments.
+2. DriverFactory class:
+	Which has the threadlocal driver instances for the parallel running capability
---
+++ TESTPROJECT/src/main/java/Utilities/TestBase.java
@@ -0,0 +1,34 @@
+package Utilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class TestBase {
+	public static String hubURL = "https://hub.lambdatest.com/wd/hub";
+    
+	public synchronized void setUp(String browser) throws MalformedURLException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", browser);
+        capabilities.setCapability("browserVersion", "112");
+        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+        ltOptions.put("user", "akshithrao19");
+        ltOptions.put("accessKey", "QKe4LBKo3l5ncqj0zmoiPe9d9cVr4vlYwu0woGBPPgcYuYetGX");
+        ltOptions.put("build", "Selenium 4");
+        ltOptions.put("name", this.getClass().getName());
+        ltOptions.put("platformName", "Windows 10");
+        ltOptions.put("seCdp", true);
+        ltOptions.put("selenium_version", "4.0.0");
+        capabilities.setCapability("LT:Options", ltOptions);
+
+        DriverFactory.getInstances().setDriver(new RemoteWebDriver(new URL(hubURL), capabilities));
+	}
+	
+	public WebDriver driver() {
+		return DriverFactory.getInstances().getDriver();
+	}
+}
